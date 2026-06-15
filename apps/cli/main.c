cbea683542e62bcbe5f4561e9819838a6d538463
qmangeard <289395329+qmangeard@users.noreply.github.com>
2026-06-15 14:33:56 +0200

    add wile loop in main and command user

--- apps/cli/main.c
+++ apps/cli/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 #include "enigma/input.h"
+
+int is_line_egal(char *, char *);
 
 int main(void) {
     printf("%s", "==================================\n");
     printf("%s", "Welcom to enigma machine simulator\n");
+    while(1){
+        char * line = NULL;
+        size_t line_size = 0;
 
+        printf("%s", "> ");
+        if(getline(&line, &line_size, stdin) == -1){
+            break;
+        }
 
+        if(is_line_egal(line, ":quit")){
+            break;
+        }
+
+        // TODO launch enigma engine here ! (enigma.c)
+    }
+    
     return 0;
+}
+
+int is_line_egal(char * line, char * command){
+    size_t lenght = strlen(command);
+    return strncmp(line, command, lenght) == 0;
 }
